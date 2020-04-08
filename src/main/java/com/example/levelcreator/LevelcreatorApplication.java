6ad68ac442a042d1ea68cb4e951af465ccd8606f
Jiarong Chen <jiarong.chen@stonybrook.edu>
2020-04-08 19:37:07 -0400

    initial version
    generated with spring initializr,, dependencies see pom.xml

---
+++ src/main/java/com/example/levelcreator/LevelcreatorApplication.java
@@ -0,0 +1,13 @@
+package com.example.levelcreator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LevelcreatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LevelcreatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/levelcreator/LevelcreatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.levelcreator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LevelcreatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
