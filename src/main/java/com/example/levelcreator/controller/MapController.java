4b19dfb35a39fc1d948647a8c2bd572b32066633
chenk97 <42250055+chenk97@users.noreply.github.com>
2020-05-01 23:59:26 -0400

    configuring literally canvas
    now can use literally canvas!

--- src/main/java/com/example/levelcreator/controller/MapController.java
+++ src/main/java/com/example/levelcreator/controller/MapController.java
@@ -1,8 +1,15 @@
 package com.example.levelcreator.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class MapController {
+
+    @RequestMapping("/customizeTileset")
+    public String literallyCanvas(){
+        return "lc.html";
+    }
+
 
 }
