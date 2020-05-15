dd55dc20bac07192351c47cd3110e5cf310bb58a
chenk97 <42250055+chenk97@users.noreply.github.com>
2020-05-14 22:46:47 -0400

    push for branch switch

--- src/main/resources/static/js/fabricCreateMap.js
+++ src/main/resources/static/js/fabricCreateMap.js
@@ -345,3 +345,13 @@ gridCanvas.on('mouse:up', function(event) {
     this.isDragging = false;
     this.selection = true;
 });
+
+function reloadTest(){
+    console.log("reload test........");
+    var json = '{"objects":[{"type":"rect","originX":"center","originY":"center","left":300,"top":150,"width":150,"height":150,"fill":"#29477F","overlayFill":null,"stroke":null,"strokeWidth":1,"strokeDashArray":null,"strokeLineCap":"butt","strokeLineJoin":"miter","strokeMiterLimit":10,"scaleX":1,"scaleY":1,"angle":0,"flipX":false,"flipY":false,"opacity":1,"shadow":{"color":"rgba(94, 128, 191, 0.5)","blur":5,"offsetX":10,"offsetY":10},"visible":true,"clipTo":null,"rx":0,"ry":0,"x":0,"y":0},{"type":"circle","originX":"center","originY":"center","left":300,"top":400,"width":200,"height":200,"fill":"rgb(166,111,213)","overlayFill":null,"stroke":null,"strokeWidth":1,"strokeDashArray":null,"strokeLineCap":"butt","strokeLineJoin":"miter","strokeMiterLimit":10,"scaleX":1,"scaleY":1,"angle":0,"flipX":false,"flipY":false,"opacity":1,"shadow":{"color":"#5b238A","blur":20,"offsetX":-20,"offsetY":-10},"visible":true,"clipTo":null,"radius":100}],"background":""}'
+    gridCanvas.loadFromJSON(json, gridCanvas.renderAll.bind(gridCanvas), function(o, object) {
+        fabric.log(o, object);
+    });
+}
+
+document.getElementById("undoBtn").addEventListener("click", reloadTest);
