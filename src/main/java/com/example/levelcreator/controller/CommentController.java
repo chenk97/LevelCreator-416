a4fb8dbb1b411592ef6d7dd5c41ce4c444ec063e
jisukim7 <32554262+jisukim7@users.noreply.github.com>
2020-05-19 18:38:31 -0400

    changes to comment database

--- src/main/java/com/example/levelcreator/controller/CommentController.java
+++ src/main/java/com/example/levelcreator/controller/CommentController.java
@@ -5,6 +5,9 @@ import com.example.levelcreator.model.Project;
 import com.example.levelcreator.service.CommentService;
 import com.example.levelcreator.service.ProjectService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
@@ -32,7 +35,22 @@ public class CommentController {
         return "redirect:/project/{id}";
     }
 
-    // @GetMapping("/saveComment")
+    @GetMapping(value = "/delComment/{id}")
+    public String delete(@PathVariable int id) {
+        Comment comment  = commentService.getCommentById(id);
+        commentService.deleteComment(id);
+        return "redirect:/project/{id}";
+    }
+
+    @GetMapping(value = "/download/{id}")
+    public ResponseEntity download(@PathVariable int id) {
+        Project proj = projectService.getProjectById(id);
+        return ResponseEntity.ok()
+                .contentType(MediaType.parseMediaType("application/json"))
+                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename = map.json")
+                .body(proj.getMapJSON());
+    }
+}
 
 
 }
